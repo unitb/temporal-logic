02d1915ef4155ec1c6e4fa586bea3f94303130cf
Simon Hudon <simon.hudon@gmail.com>
2018-01-11 12:09:05 -0500

    added a general theorem about data refinement (with forward simulation, without fairness)

--- src/temporal_logic/lemmas.lean
+++ src/temporal_logic/lemmas.lean
@@ -3,6 +3,7 @@ import data.stream
 
 import util.meta.tactic
 import util.logic
+import util.classical
 import util.predicate
 
 import temporal_logic.tactic
@@ -681,7 +682,7 @@ parameters INV : ∀ σ σ', σ ⊨ J → A σ σ' → σ' ⊨ J
 open classical
 
 include H₀ INV
-noncomputable def w : ℕ → { σ // σ ⊨ J }
+private noncomputable def w : ℕ → { σ // σ ⊨ J }
   | 0 :=
 let x₀ := some FIS₀ in
 have H₀ : x₀ ⊨ J,
@@ -713,7 +714,7 @@ begin
   simp,
   existsi (w' H₀ FIS₀ FIS INV x),
   split,
-  { simp [w',nat.sub_self,w],
+  { simp [w',nat.sub_self],
     apply classical.some_spec },
   { simp_intros [henceforth,action,w',nat.add_sub_cancel_left],
     rw [← add_succ,nat.add_sub_cancel_left],
@@ -724,4 +725,134 @@ begin
 end
 
 end witness_construction
+
+section simulation
+
+section pair
+
+variables {α' : Type u} {β' : Type u₀} {γ' : Type u₁}
+variables (x : tvar α') (y : tvar β') (z : tvar γ')
+
+def pair : tvar (α' × β') :=
+lifted₂ prod.mk x y
+
+notation `⦃` x `,` l:(foldl `,` (h t, pair h t) x `⦄`)  := l
+
+@[simp]
+lemma pair_model (i : ℕ) :
+i ⊨ ⦃x,y⦄ = (i ⊨ y,i ⊨ x) :=
+by { cases x, cases y, refl }
+
+end pair
+
+parameters {α' : Type u} {β' : Type u₀} {γ' : Type u₁ }
+parameters {p : pred' (α'×γ')} {q : pred' (β'×γ')}
+parameters {A : act (α'×γ')} {C : act (β'×γ')}
+parameters {J : pred' (α'×β'×γ')}
+
+variables (x : tvar α') (y : tvar β') (z : tvar γ')
+
+def SPEC₀ (v : tvar α') (o : tvar γ') : cpred :=
+p ;; ⦃ o,v ⦄ ⋀
+◻⟦ ⦃o,v⦄ <> A ⟧
+
+def SPEC₁ (v : tvar β') (o : tvar γ') : cpred :=
+q ;; ⦃ o,v ⦄ ⋀
+◻⟦ ⦃o,v⦄ <> C ⟧
+
+parameter [inhabited α']
+parameter SIM₀ : ∀ v o, (v,o) ⊨ q → ∃ w, (w,o) ⊨ p ∧ (w,v,o) ⊨ J
+parameter SIM
+: ∀ {w v o} v' o',
+  (w,v,o) ⊨ J →
+  C (v,o) (v',o') →
+  ∃ w', A (w,o) (w',o') ∧
+        (w',v',o') ⊨ J
+
+parameters (v : tvar β') (o : tvar γ')
+
+parameters {Γ : cpred}
+parameters H : Γ ⊢ SPEC₁ v o
+
+section witness
+variables (i : ℕ) (Hi : i ⊨ Γ)
+include H Hi
+
+open classical
+
+private noncomputable def ww : Π j : ℕ, { w : α' // j ≥ i → (w,j ⊨ v,j ⊨ o) ⊨ J }
+ | j :=
+if h : j > i then
+     have h₀ : j ≥ 1,
+       by { apply succ_le_of_lt,
+            apply nat.lt_of_le_of_lt (nat.zero_le i) h },
+     have h₁ : i ≤ j - 1,
+       by { rw ← add_le_to_le_sub _ ; assumption },
+     have Hdec : j - 1 < j,
+       by { apply nat.sub_lt_of_pos_le, apply zero_lt_one,
+            assumption },
+     let hw := (ww (j-1)).property h₁ in
+     have Hstep : C (j - 1 ⊨ v, j - 1 ⊨ o) (j ⊨ v, j ⊨ o),
+       begin
+         clear_except H Hi h₀ h₁ h, replace H := (H.apply _ Hi).right ((j-1) - i),
+         simp at H, rw [← nat.add_sub_assoc,nat.add_sub_cancel_left,← succ_sub] at H
+         ; apply H <|> assumption,
+       end,
+     let x := some (SIM (j ⊨ v) (j ⊨ o) hw Hstep) in
+     have h₁ : j ≥ i → (x, j ⊨ v, j ⊨ o) ⊨ J,
+       from assume _, (some_spec (SIM (j ⊨ v) (j ⊨ o) hw Hstep)).right,
+     ⟨x,h₁⟩
+else if h' : i ≤ j then
+     have h : i = j,
+       by { apply le_antisymm h', apply le_of_not_gt, assumption },
+     have h₀ : (j ⊨ v, j ⊨ o) ⊨ q,
+          begin
+            clear_except H Hi h,
+            have := H.left.apply _ Hi, simp at this,
+            subst i, apply this,
+          end,
+     let x₀ := some (SIM₀ (j ⊨ v) (j ⊨ o) h₀) in
+     have h₁ : j ≥ i → (x₀, j ⊨ v, j ⊨ o) ⊨ J,
+       from assume _, (some_spec (SIM₀ (j ⊨ v) (j ⊨ o) h₀)).right,
+     ⟨x₀,h₁⟩
+else
+  have h₁ : j ≥ i → (default α', j ⊨ v, j ⊨ o) ⊨ J,
+    by { intro, exfalso, apply h', assumption, },
+  ⟨default α',h₁⟩
+
+private noncomputable def ww' (j : ℕ) := (ww i Hi j).val
+
+end witness
+include H SIM₀ SIM
+
+lemma simulation
+: Γ ⊢ ∃∃ w, SPEC₀ w o :=
+begin
+  simp [SPEC₀],
+  lifted_pred keep,
+  let w := ww' SIM₀ @SIM v o H _ a,
+  existsi (⟨ w ⟩ : tvar α'),
+  split,
+  intro i,
+  { simp,
+    generalize h : w (x + i) = z,
+    have h' : succ (x + i) > x,
+    { apply lt_succ_of_le, apply nat.le_add_right },
+    simp [w], rw [ww',ww], simp [dif_pos h'],
+    rw ← h,
+    apply_some_spec, intros, assumption, },
+  { dsimp [w,ww'], rw ww, simp, apply_some_spec,
+    intros, assumption, },
+end
+omit H
+lemma simulation'
+: (∃∃ c, SPEC₁ c o) ⟹ (∃∃ a, SPEC₀ a o) :=
+begin [temporal]
+  rw p_exists_p_imp,
+  intros x h,
+  apply simulation SIM₀ @SIM _ _ h,
+end
+
+end simulation
+
 end temporal
